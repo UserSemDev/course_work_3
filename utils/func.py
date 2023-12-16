06e6f024383e6e57acccff3f609ca4ef45cd7972
Sem <semseveryanin@gmail.com>
2023-12-16 19:38:20 +0300

    Добавлена функция get_filter_json_file

--- utils/func.py
+++ utils/func.py
@@ -9,3 +9,8 @@ def load_json_file(json_file):
         return data
     else:
         return "Файл json не найден"
+
+
+def get_filter_json_file(data):
+    sorted_data = sorted([i for i in data if i], key=lambda x: x.get('date'), reverse=True)
+    return sorted_data

117513d986da5103cd67fd243869d335032d4d78
Sem <semseveryanin@gmail.com>
2023-12-16 20:25:44 +0300

    Добавлен тест test_get_filter_json_file

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,6 +1,6 @@
 import os
 from config import TEST_JSON_PATH
-from utils.func import load_json_file
+from utils.func import load_json_file, get_filter_json_file
 
 
 def test_load_json_file():
@@ -10,3 +10,12 @@ def test_load_json_file():
     assert load_json_file(path2) == []
     path3 = os.path.join(TEST_JSON_PATH, 'file_not_found.json')
     assert load_json_file(path3) == "Файл json не найден"
+
+
+def test_get_filter_json_file():
+    data = [{"date": "2018-06-30T02:08:58.425572"},
+            {"date": "2019-07-03T18:35:29.512364"},
+            {"date": "2019-08-26T10:50:58.294041"}]
+    assert get_filter_json_file(data) == [{"date": "2019-08-26T10:50:58.294041"},
+                                          {"date": "2019-07-03T18:35:29.512364"},
+                                          {"date": "2018-06-30T02:08:58.425572"}]
